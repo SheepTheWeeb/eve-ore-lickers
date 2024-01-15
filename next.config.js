64d754b808d0547f56d4b0cd4d05c55f327ae5c6
DESKTOP-8T462RT\jerry <jerryblaauw@hotmail.com>
2024-01-15 18:11:02 +0100

    metadata update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['images.evetech.net'],
+      },
+}
 
 module.exports = nextConfig
