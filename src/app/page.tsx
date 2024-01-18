c40431d5f14a986f4bfd3fc97659d74ea7ad9653
DESKTOP-8T462RT\jerry <jerryblaauw@hotmail.com>
2024-01-18 22:34:37 +0100

    fixed contentful homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,65 @@
 
 import { HomeController } from "@/controllers/home-controller";
 import Image from "next/image";
+import { contentfulSettings } from "@/settings/contentful-settings";
+import { Alert } from "flowbite-react";
+import { HiInformationCircle } from "react-icons/hi";
+
+const settings = contentfulSettings[process.env.NODE_ENV];
 
 export default function Home() {
-  const { data, isLoading } = HomeController().useHomePageContent();
+  const { data, isLoading, error } = HomeController().useHomePageContent({
+    id: settings.homePageContentId,
+  });
+
+  if (error) {
+    return (
+      <Alert color="failure" icon={HiInformationCircle}>
+        <span className="font-medium">Error:</span>
+        {" Something went wrong when fetching homepage data."}
+      </Alert>
+    );
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <main className="flex flex-col">
-      {isLoading || !data ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          <div className="pb-5">
-            <h1>{data.title}</h1>
-          </div>
-          <div className="leading-[2em] mb-10">
-            <p
-              className="content"
-              dangerouslySetInnerHTML={{ __html: data.homeText }}
-            />
-          </div>
-          <div className="flex justify-center">
+      <div>
+        <div className="pb-5">
+          <h1>{data?.title}</h1>
+        </div>
+        {data?.imageOne && (
+          <div className="flex justify-center pb-5">
             <Image
-              src={data.image.desktop.url}
-              alt="logo"
-              width={200}
-              height={200}
+              src={data.imageOne.desktop.url}
+              alt="image1"
+              width={data.imageOne.desktop.width}
+              height={data.imageOne.desktop.height}
               priority
             />
           </div>
+        )}
+        <div className="leading-[2em] mb-10">
+          <p
+            className="content"
+            dangerouslySetInnerHTML={{ __html: data?.contentBlock! }}
+          />
         </div>
-      )}
+        {data?.imageTwo && (
+          <div className="flex justify-center">
+            <Image
+              src={data.imageTwo.desktop.url}
+              alt="image2"
+              width={data.imageTwo.desktop.width}
+              height={data.imageTwo.desktop.height}
+              priority
+            />
+          </div>
+        )}
+      </div>
     </main>
   );
 }
--- src/gateways/cms/contentful-gateway.ts
+++ src/gateways/cms/contentful-gateway.ts
@@ -1,28 +1,27 @@
 import { ContentfulService } from "@/domain/cms/contentful-service";
-import { HomePageContent } from "@/domain/cms/home";
-import { contentfulSettings } from "@/settings/contentful-settings";
+import { StaticPageContent } from "@/domain/cms/static-page";
 import { LOCALES } from "@/utils/constants/locales";
-import { mapHomePageContent } from "@/utils/mappers/homepage-mapper";
+import { mapStaticPageContent } from "@/utils/mappers/staticpage-mapper";
 import { createClient } from "contentful";
 
-const settings = contentfulSettings[process.env.NODE_ENV];
 const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!,
   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN!,
 });
 
 const ContentfulGateway = (): ContentfulService => {
-  const getHomePageContent = async (
+  const getStaticPageContent = async (
+    id: string,
     locale = LOCALES.DEFAULT
-  ): Promise<HomePageContent> => {
-    const homeResponse = await client.getEntry(settings.homePageContentId, {
+  ): Promise<StaticPageContent> => {
+    const homeResponse = await client.getEntry(id, {
       locale,
     });
-    return mapHomePageContent(homeResponse);
+    return mapStaticPageContent(homeResponse);
   };
 
   return {
-    getHomePageContent,
+    getStaticPageContent,
   };
 };
 
