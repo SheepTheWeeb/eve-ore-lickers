04de8d6d7038efe604343548fc6ec991876552ab
DESKTOP-8T462RT\jerry <jerryblaauw@hotmail.com>
2024-01-26 22:05:08 +0100

    changed hamburger menu icon

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@
 import { useUIStore } from "@/store/uiStore";
 import Link from "next/link";
 import { useEffect, useRef } from "react";
-import { FaHouseChimney, FaUsers } from "react-icons/fa6";
+import { FaBars, FaHouseChimney, FaUsers } from "react-icons/fa6";
 
 function useClickOutside(ref: any, onClickOutside: Function) {
   useEffect(() => {
@@ -35,19 +35,7 @@ export default function Header() {
         className="inline-flex items-center p-2 mt-2 ms-3 text-sm rounded-lg sm:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
       >
         <span className="sr-only">Open sidebar</span>
-        <svg
-          className="w-6 h-6"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            clipRule="evenodd"
-            fillRule="evenodd"
-            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-          ></path>
-        </svg>
+        <FaBars className="w-6 h-6" />
       </button>
 
       <aside
