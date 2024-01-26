09238a2f7b6af5d9728e0909e5dfeaf9f0d2e65b
DESKTOP-8T462RT\jerry <jerryblaauw@hotmail.com>
2024-01-26 21:53:54 +0100

    changed icons in header and fixed table scrolling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,6 +3,7 @@
 import { useUIStore } from "@/store/uiStore";
 import Link from "next/link";
 import { useEffect, useRef } from "react";
+import { FaHouseChimney, FaUsers } from "react-icons/fa6";
 
 function useClickOutside(ref: any, onClickOutside: Function) {
   useEffect(() => {
@@ -64,15 +65,7 @@ export default function Header() {
                 className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 group"
                 onClick={closeNavigation}
               >
-                <svg
-                  className="w-5 h-5 transition duration-75 text-gray-400 group-hover:text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 26 26"
-                >
-                  <path d="M 20 2.03125 C 19.449219 2.03125 19 2.480469 19 3.03125 L 19 7.8125 L 13.71875 2.53125 C 13.328125 2.140625 12.671875 2.140625 12.28125 2.53125 L 0.5625 14.28125 C 0.171875 14.671875 0.171875 15.296875 0.5625 15.6875 C 0.953125 16.078125 1.578125 16.078125 1.96875 15.6875 L 13 4.65625 L 24.0625 15.71875 C 24.257813 15.914063 24.523438 16.03125 24.78125 16.03125 C 25.039063 16.03125 25.273438 15.914063 25.46875 15.71875 C 25.859375 15.328125 25.859375 14.703125 25.46875 14.3125 L 22 10.84375 L 22 3.03125 C 22 2.480469 21.550781 2.03125 21 2.03125 Z M 13 6.5 L 2 17.5 L 2 23 C 2 24.65625 3.34375 26 5 26 L 21 26 C 22.65625 26 24 24.65625 24 23 L 24 17.5 Z M 11 16 L 15 16 C 15.550781 16 16 16.449219 16 17 L 16 23 C 16 23.550781 15.550781 24 15 24 L 11 24 C 10.449219 24 10 23.550781 10 23 L 10 17 C 10 16.449219 10.449219 16 11 16 Z"></path>
-                </svg>
+                <FaHouseChimney className="w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white" />
                 <span className="ms-3">Home</span>
               </Link>
             </li>
@@ -82,15 +75,7 @@ export default function Header() {
                 className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 group"
                 onClick={closeNavigation}
               >
-                <svg
-                  className="flex-shrink-0 w-5 h-5 transition duration-75 text-gray-400 group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 18 18"
-                >
-                  <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                </svg>
+                <FaUsers className=" w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white" />
                 <span className="flex-1 ms-3 whitespace-nowrap">About us</span>
               </Link>
             </li>
